4f06ce496a649830526dccddc2d7214b533cad9c
koohee <ko_ohee@naver.com>
2022-08-28 21:37:53 +0900

    Initial Commit

---
+++ Outstargram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Outstargram
+//
+//  Created by 구희정 on 2022/08/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
